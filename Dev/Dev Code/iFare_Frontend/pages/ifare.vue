b7c5d63477e8feacd63202267c435a7dbcb4b027
Chainwinit <chainwinit@gmail.com>
2026-05-04 17:05:48 +0800

    feat(v1.0.3): 新增模糊搜尋功能並優化搜尋介面UI

--- Dev/Dev Code/iFare_Frontend/pages/ifare.vue
+++ Dev/Dev Code/iFare_Frontend/pages/ifare.vue
@@ -73,7 +73,7 @@
           </div>
           <div class="item item-query">
             <label class="filter-name">關鍵字</label>
-            <input v-model="searchQuery" class="input-query" type="text" placeholder="輸入繁中關鍵字" />
+            <input v-model="searchQuery" class="input-query" type="text" placeholder="請輸入關鍵字" />
           </div>
           <div class="item item-bottom">
             <button class="btn-filter transition-general" @click="Search" :disabled="!canSearch">
@@ -195,6 +195,9 @@ interface selectItem {
   isActive: boolean;
 }
 
+const ALL_POLICY_VALUE = "__all_policy";
+const ALL_AREA_VALUE = "__all_area";
+
 function isSelectOpen(type: string, val: boolean) {
   // console.log(`[${type}] val => ${val} || type ${typeof val}`)
   _isSelect.value = val
@@ -243,14 +246,14 @@ codePolicy.then((res: any) => {
   if (!res?.result?.result) return;
   const _data = res.result.result;
 
-  let _list: Array<selectItem> = _data.map((item: any, i: number) => {
+let _list: Array<selectItem> = _data.map((item: any, i: number) => {
     return {
       name: item.codeName,
       val: item.id,
     };
   });
 
-  policySelectList.push(..._list);
+  policySelectList.push({ name: "全部", val: ALL_POLICY_VALUE, isActive: false }, ..._list);
 });
 
 // Code area
@@ -259,14 +262,14 @@ codeArea.then((res: any) => {
   if (!res?.result?.result) return;
   const _data = res.result.result;
 
-  let _list: Array<selectItem> = _data.map((item: any, i: number) => {
+let _list: Array<selectItem> = _data.map((item: any, i: number) => {
     return {
       name: item.codeName,
       val: item.id,
     };
   });
 
-  areaSelectList.push(..._list);
+  areaSelectList.push({ name: "全國", val: ALL_AREA_VALUE, isActive: false }, ..._list);
 });
 
 // Code recipient
@@ -287,6 +290,13 @@ codeRecipient.then((res: any) => {
 });
 
 function SwitchRecipient(codeVal: any) {
+  const selectedItem = recipientSelectList.find((item) => item.val == codeVal);
+  if (selectedItem?.isActive) {
+    selectedItem.isActive = false;
+    codeSelectRecipient.value = "";
+    return;
+  }
+
   recipientSelectList.forEach((item, i) => {
     item.isActive = item.val == codeVal;
     if (item.isActive) {
